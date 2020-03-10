6883352fca36540b8e1f6ac626875df025e7fdf8
Erik Saulnier <me@eriksaulnier.com>
2020-03-10 19:30:42 -0400

    Fixed the Dude character's backpack

--- src/js/graphics/Dude.js
+++ src/js/graphics/Dude.js
@@ -13,8 +13,8 @@ import {
 } from '../colors';
 
 export const jettingPixels = [
-  ['transparent', 'transparent', 'transparent', 'transparent', 'transparent', colorLightMetal, colorLightMetal, colorLightMetal, colorDarkMetal, colorDarkMetal, 'transparent', 'transparent', 'transparent'],
   ['transparent', 'transparent', 'transparent', 'transparent', 'transparent', 'transparent', colorLightMetal, colorLightMetal, colorDarkMetal, 'transparent', 'transparent', 'transparent', 'transparent'],
+  ['transparent', 'transparent', 'transparent', 'transparent', 'transparent', colorLightMetal, colorLightMetal, colorLightMetal, colorDarkMetal, colorDarkMetal, 'transparent', 'transparent', 'transparent'],
   ['transparent', colorDarkBrown, colorDarkBrown, colorDarkBrown, colorLightTan, colorDarkGreen, colorDarkGreen, colorDarkGreen, colorLightGreen, colorMediumBlue, colorMediumBlue, colorMediumBlue, colorBlack],
   ['transparent', colorDarkBrown, colorDarkBrown, colorLightTan, colorLightTan, colorLightGreen, colorDarkGreen, colorDarkGreen, colorLightGreen, colorMediumBlue, colorMediumBlue, colorMediumBlue, colorBlack],
   ['transparent', colorDarkBrown, colorLightTan, colorWhite, colorLightTan, colorLightGreen, colorLightGreen, colorDarkGreen, colorLightGreen, colorDarkBlue, colorDarkBlue, colorDarkBlue, colorBlack],
